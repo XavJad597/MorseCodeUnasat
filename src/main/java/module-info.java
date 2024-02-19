90cb2d343678efcec0356d32e30e78d9833e151e
XavJad597 <92932067+XavJad597@users.noreply.github.com>
2024-02-19 13:20:11 -0300

    Modules updated

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,14 @@
 module com.unasat.morsecodeunasat {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-    requires javafx.media;
+        requires javafx.controls;
+        requires javafx.fxml;
+        requires javafx.web;
+        requires javafx.media;
+        requires org.controlsfx.controls;
+        requires net.synedra.validatorfx;
+        requires org.kordamp.ikonli.javafx;
+        requires org.kordamp.bootstrapfx.core;
+        requires eu.hansolo.tilesfx;
 
-    requires org.controlsfx.controls;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-
-    opens com.unasat.morsecodeunasat to javafx.fxml;
-    exports com.unasat.morsecodeunasat;
-}
+        opens com.unasat.morsecodeunasat to javafx.fxml;
+        exports com.unasat.morsecodeunasat;
+        }

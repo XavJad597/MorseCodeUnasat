28c05e1e1609d88fee14478ac024c0f41184cb38
XavJad597 <92932067+XavJad597@users.noreply.github.com>
2024-02-22 01:39:07 -0300

    GUI updated and text tested(english to morse)

--- src/main/java/com/unasat/morsecodeunasat/HelloController.java
+++ src/main/java/com/unasat/morsecodeunasat/HelloController.java
@@ -2,13 +2,28 @@ package com.unasat.morsecodeunasat;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private TextArea englishTextArea; // Reference to the TextArea for English text input
+
+    @FXML
+    private TextArea morseTextArea; // Reference to the TextArea for Morse text output
+
+    @FXML
+    private MorseCodeTranslator translator = new MorseCodeTranslator();
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        String englishText = englishTextArea.getText();
+
+        // Call the English to Morse function from the Translator class
+        String morseText = translator.englishToMorse(englishText);
+
+        // Set the Morse text to the TextArea for Morse output
+        morseTextArea.setText(morseText);
     }
 }
